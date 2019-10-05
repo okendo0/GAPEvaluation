e532a815202b17801350277dc9f3a32f1570d1f8
okendo0 <54463496+okendo0@users.noreply.github.com>
2019-10-05 00:45:01 -0500

    Project Creation

---
+++ GAPEvaluation.API/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace GAPEvaluation.API.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ GAPEvaluation/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GAPEvaluation.Startup))]
+namespace GAPEvaluation
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
